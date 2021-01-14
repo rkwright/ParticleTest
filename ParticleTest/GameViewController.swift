7a73f93eb013cf4a13a03efcaa0c8fbd1406d5a7
rkwright <rkwright@geofx.com>
2021-01-14 11:17:46 -0800

    Completed the refactoring.  Demo is effectively complete.

--- ParticleTest/GameViewController.swift
+++ ParticleTest/GameViewController.swift
@@ -15,6 +15,7 @@ import SceneKit
 class GameViewController: UIViewController {
 
     
+    
     /**
      *  Use the viewDidLoad() overrride to construct our scene.
      */
@@ -22,36 +23,19 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         let scene = createScene()
+        
         createCamera(scene)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively:true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        createLights(scene)
+
+        let ship = fetchShip(scene)
        
-        //var shapeNode:SCNNode
-        var geometry:SCNGeometry
+        let geometry:SCNGeometry = SCNSphere(radius: 1.0)
         
-        geometry = SCNSphere(radius: 1.0)
-        //shapeNode = SCNNode(geometry: geometry)
         geometry.materials.first?.diffuse.contents = UIColor.red
 
         let trailEmitter = createTrail(color: UIColor.red, geometry: geometry)
+       
         ship.addParticleSystem(trailEmitter)
         
         // retrieve the SCNView
@@ -74,13 +58,33 @@ class GameViewController: UIViewController {
         scnView.addGestureRecognizer(tapGesture)
     }
     
-    fileprivate func createScene() -> SCNScene {
+    /**
+     * Create the scene by loading it from the app's resources
+     */
+    func createScene () -> SCNScene {
         // create a new scene
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
         return scene
     }
     
-    fileprivate func createCamera(_ scene: SCNScene) {
+    /**
+     * Load the ship from the app's resources then set it rotating
+     */
+    func fetchShip ( _ scene: SCNScene ) ->SCNNode {
+        // retrieve the ship node
+        let ship = scene.rootNode.childNode(withName: "ship", recursively:true)!
+        
+        // animate the 3d object
+        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+
+        return ship
+    }
+    
+    /**
+     * Just create a single camera and point it at the scene
+     */
+    func createCamera ( _ scene: SCNScene ) {
         // create and add a camera to the scene
         let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
@@ -90,8 +94,47 @@ class GameViewController: UIViewController {
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
     }
 
+    /**
+     *  Create the lights for the scene.
+     *  Should return a tuple.  TODO
+     */
+    func createLights ( _ scene: SCNScene ) {
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+    }
+
+    /**
+     * Create the trail system in memory by loading the file (actually in app's resources)
+     */
+    func createTrail ( color: UIColor, geometry: SCNGeometry ) -> SCNParticleSystem {
+        
+      // Fetch the particle system from the file
+      let trail = SCNParticleSystem(named: "Trail.scnp", inDirectory: nil)!
+        
+      // set the color and geometry
+      trail.particleColor = color
+      trail.emitterShape = geometry
+      
+      return trail
+    }
+    
+    /**
+     * Tap handler
+     */
     @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+    func handleTap ( _ gestureRecognize: UIGestureRecognizer ) {
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
@@ -126,6 +169,9 @@ class GameViewController: UIViewController {
         }
     }
     
+    /**
+     * Handle the device orientation changes
+     */
     override var shouldAutorotate: Bool {
         return true
     }
@@ -140,17 +186,5 @@ class GameViewController: UIViewController {
         } else {
             return .all
         }
-    }
-
-    func createTrail(color: UIColor, geometry: SCNGeometry) -> SCNParticleSystem {
-        
-      // 2
-      let trail = SCNParticleSystem(named: "Trail.scnp", inDirectory: nil)!
-      // 3
-      trail.particleColor = color
-      // 4
-      trail.emitterShape = geometry
-      // 5
-      return trail
     }
 }
